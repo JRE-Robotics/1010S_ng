fa6fa26280885591e4f07a91f608c56522268f09
Joe Dai <dm.joedai@gmail.com>
2022-03-05 16:07:21 -0800

    New upload script to upload all autonomous modes

--- upload.bat
+++
@@ -1,1 +0,0 @@
- prosv5 mu --slot 1 --name 1010S_ng
---
+++ upload_all.ps1
@@ -0,0 +1,41 @@
+﻿# Modes: AWP, BALANCE, RUSH, NONE, SKILLS
+
+Write-Progress -Activity '1010S Auto Uploader' -PercentComplete 0
+
+Read-Host -Prompt "Ensure auto_mode = BALANCE [ENTER]"
+
+Write-Progress -Activity '1010S Auto Uploader' -Status "MAKE: BALANCE auto" -PercentComplete 0
+prosv5 make
+Write-Progress -Activity '1010S Auto Uploader' -Status "UPLOAD: BALANCE auto" -PercentComplete 0
+prosv5 upload --name "BALANCE" --slot 1 --run-screen
+
+Read-Host -Prompt "Ensure auto_mode = AWP [ENTER]"
+
+Write-Progress -Activity '1010S Auto Uploader' -Status "MAKE: AWP auto" -PercentComplete 20
+prosv5 make
+Write-Progress -Activity '1010S Auto Uploader' -Status "UPLOAD: AWP auto" -PercentComplete 20
+prosv5 upload --name "AWP" --slot 2 --run-screen
+
+Read-Host -Prompt "Ensure auto_mode = RUSH [ENTER]"
+
+Write-Progress -Activity '1010S Auto Uploader' -Status "MAKE: RUSH auto" -PercentComplete 40
+prosv5 make
+Write-Progress -Activity '1010S Auto Uploader' -Status "UPLOAD: RUSH auto" -PercentComplete 40
+prosv5 upload --name "RUSH" --slot 3 --run-screen
+
+Read-Host -Prompt "Ensure auto_mode = NONE [ENTER]"
+
+Write-Progress -Activity '1010S Auto Uploader' -Status "MAKE: NONE auto" -PercentComplete 60
+prosv5 make
+Write-Progress -Activity '1010S Auto Uploader' -Status "UPLOAD: NONE auto" -PercentComplete 60
+prosv5 upload --name "NONE" --slot 4 --run-screen
+
+Read-Host -Prompt "Ensure auto_mode = SKILLS [ENTER]"
+
+Write-Progress -Activity '1010S Auto Uploader' -Status "MAKE: SKILLS auto" -PercentComplete 80
+prosv5 make
+Write-Progress -Activity '1010S Auto Uploader' -Status "UPLOAD: SKILLS auto" -PercentComplete 80
+prosv5 upload --name "SKILLS" --slot 5 --run-screen
+
+Write-Progress -Activity '1010S Auto Uploader' -PercentComplete 100
+Write-Host "Done!"
